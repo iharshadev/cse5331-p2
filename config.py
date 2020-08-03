01b4c23c1ddea98f8fa0a0ecd9883dce9bd41d45
Harsha <harshavardhanr.993@gmail.com>
2020-08-03 17:29:45 -0500

    1. Added a method to fetch rows my MySQL as JSON formatted documents with project and employee as root
    2. Added a method to insert the JSON documents into respective collections in MongoDB

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 class MySQLProps:
     # Ensure that user has INSERT and DELETE Privileges on the tables
-    dbname = ""
-    host = ""
-    user = ""
-    password = ""
+    dbname = "project2"
+    host = "db2-sum20.cximppf4gt1a.us-east-1.rds.amazonaws.com"
+    user = "root"
+    password = "DB2atSUM2020"
+
+class MongoProps:
+    user = "p2demo"
+    host = "db2.tmzpw.mongodb.net"
+    dbname = "project2"
+    password = "DB2atSUM2020"
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import csv
+import json
 import re
 
+import pymongo
 import pymysql
 
-from config import MySQLProps as props
+from config import MongoProps as mp
+from config import MySQLProps as sql_props
 
 
 def clear_tables():
     tables_in_order = ["WORKS_ON", "DEPT_LOCATIONS", "PROJECT", "EMPLOYEE", "DEPARTMENT"]
     try:
-        with connection.cursor() as cursor:
+        with mysql_client.cursor() as cursor:
             [cursor.execute(f"DELETE FROM {table};") for table in tables_in_order]
-            connection.commit()
-            print("All tables emptied successfully")
+            mysql_client.commit()
+            print("All tables in MySQL emptied successfully")
     except pymysql.MySQLError:
         print(f"Error while emptying tables")
     finally:
@@ -25,10 +28,10 @@ def load_data(table):
         records = [format_insert_query(record, table) for record in reader]
         insert_query = f"INSERT INTO {table} VALUES {','.join(records)};"
         try:
-            with connection.cursor() as cursor:
+            with mysql_client.cursor() as cursor:
                 cursor.execute(insert_query)
-                connection.commit()
-                print(f"Table: {table} populated successfully")
+                mysql_client.commit()
+                print(f"MySQL table - {table} populated successfully")
         except pymysql.MySQLError as e:
             print("Error while establishing connection", e)
         finally:
@@ -51,33 +54,48 @@ def format_insert_query(record, tablename):
         return f"({','.join(record)})"
 
 
-def load_to_mongodb(root):
+def fetch_as_document(root):
     document_query = None
     with open(f"scripts/{root}-as-root.sql", "rt") as query_file:
         document_query = query_file.read()
     try:
-        with connection.cursor() as cursor:
+        with mysql_client.cursor() as cursor:
             cursor.execute(document_query)
-            documents = cursor.fetchall()
-            for document in documents:
-                print(document[0])
+            return [json.loads(row[0]) for row in cursor.fetchall()]
+    except pymysql.MySQLError as e:
+        print(f"Error while fetching data from MySQL in document format {root} as root", e)
+    finally:
+        cursor.close()
 
-    except pymysql.MySQLError:
-        print("Error while retrieving project document from MySQL")
+
+def load_to_mongodb(documents, collection_name):
+    # collection names : [projects, employees]
+    collection = mongo_client.db2[f"{collection_name}s"]
+    collection.delete_many({})
+    print(f"MongoDB collection {collection_name}s emptied for fresh insertion")
+    [collection.insert_one(document) for document in documents]
+    print(f"{len(documents)} documents inserted to the collection: {collection_name}s in MongoDB successfully")
 
 
-connection = pymysql.connect(host=props.host,
-                             user=props.user,
-                             password=props.password,
-                             database=props.dbname)
+mysql_client = pymysql.connect(host=sql_props.host,
+                               user=sql_props.user,
+                               password=sql_props.password,
+                               database=sql_props.dbname)
+
+mongo_client = pymongo.MongoClient(f"mongodb+srv://{mp.user}:{mp.password}@{mp.host}/{mp.dbname}")
+
 # empties the tables so data is freshly populated
 clear_tables()
+
+# Load data to MySQL tables. ORDER IS IMPORTANT
 load_data("DEPARTMENT")
 load_data("DEPT_LOCATIONS")
 load_data("EMPLOYEE")
 load_data("PROJECT")
 load_data("WORKS_ON")
 
-load_to_mongodb("project")
-print("//////////////////////")
-load_to_mongodb("employee")
+project_documents = fetch_as_document("project")
+load_to_mongodb(project_documents, "project")
+
+employee_documents = fetch_as_document("employee")
+load_to_mongodb(employee_documents, "employee")
